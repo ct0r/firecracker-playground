a60fb0cbf2f29396db6f2c1f2b44aaf71ce6a684
ct0r <ct0r@protonmail.com>
2021-03-06 22:22:30 +0100

    Download kernel & rootfs in download script

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
+# firecracker binaries
 release_url="https://github.com/firecracker-microvm/firecracker/releases"
 latest=$(basename $(curl -fsSLI -o /dev/null -w %{url_effective} ${release_url}/latest))
 arch=$(uname -m)
@@ -9,3 +10,13 @@ rm -rf bin &&
     mv firecracker-${latest}-${arch} firecracker &&
     mv jailer-${latest}-${arch} jailer &&
     mv -- -${latest}.yaml swagger.yml
+
+# kernel & rootfs
+dest_kernel="hello-vmlinux.bin"
+dest_rootfs="hello-rootfs.ext4"
+image_bucket_url="https://s3.amazonaws.com/spec.ccfc.min/img"
+kernel="${image_bucket_url}/hello/kernel/hello-vmlinux.bin"
+rootfs="${image_bucket_url}/hello/fsfiles/hello-rootfs.ext4"
+
+curl -fsSL -o $dest_kernel $kernel &&
+    curl -fsSL -o $dest_rootfs $rootfs
